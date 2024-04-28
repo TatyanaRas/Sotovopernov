1651c805ac5f3df2a441a213ed263843e64e5cd3
Tatyana <tatyanalipch@yandex.ru>
2024-04-29 00:42:11 +0300

    src/Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,18 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        int a = 100;//средства на счете
-        int b = 1100;// сумма пополнения
-        int e = 0;//начальная сумма бонусов
-        int d = b / 100 * 1;//бонусы за пополнение
-        int f = e + d;//итоговая сумма бонусов
-        int c = a + b + f;//итоговая сумма на счете с бонусами
-        if (b > 1000) {
-            System.out.println(f);//колличество бонусных рублей
-            System.out.println(c);//сумма на счете
+        int srNaSchet = 100;//средства на счете
+        int sumPopln = 1100;// сумма пополнения
+        int nacSumBon = 0;//начальная сумма бонусов
+        int bonZaPopol = sumPopln / 100 * 1;//бонусы за пополнение
+        int itogSumBon = nacSumBon + bonZaPopol;//итоговая сумма бонусов
+        int sumNaScete = srNaSchet + sumPopln + itogSumBon;//итоговая сумма на счете с бонусами
+        if (sumPopln > 1000) {
+            System.out.println(itogSumBon);//колличество бонусных рублей
+            System.out.println(sumNaScete);//сумма на счете
         } else {
-            System.out.println(e);//бонусы
-            System.out.println(a + b);//сумма на счете
+            System.out.println(nacSumBon);//бонусы
+            System.out.println(srNaSchet + sumPopln);//сумма на счете
 
         }
 
